0cfaf36fa46c26d78ba95fe753833637104f9d6a
rowjoy <rowjoy202@gmail.com>
2024-04-29 14:49:39 +0600

    Initial Commit

---
+++ todoapps/todoappsApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappsApp.swift
+//  todoapps
+//
+//  Created by Nec Money on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct todoappsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
