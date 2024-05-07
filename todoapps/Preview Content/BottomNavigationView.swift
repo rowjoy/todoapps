7f02c5f12274047166119194778b1bd61ff8532b
rowjoy <rowjoy202@gmail.com>
2024-05-07 20:37:13 +0600

    Complete bottom Navigation ber

--- todoapps/Preview Content/BottomNavigationView.swift
+++ todoapps/Preview Content/BottomNavigationView.swift
@@ -10,7 +10,23 @@ import SwiftUI
 struct BottomNavigationView : View {
     var body: some View {
         TabView{
-            
+            DashBoardView().tabItem {
+                Image(systemName: "person")
+                Text("Dash board")
+            }
+            TaskView().tabItem {
+                Image(systemName: "person")
+                Text("Task")
+            }
+            ReportView().tabItem {
+                Image(systemName: "person")
+                Text("Report")
+            }
+            ProfileView().tabItem {
+                Image(systemName: "person")
+                Text("Profile")
+                
+            }
         }
         
     }
